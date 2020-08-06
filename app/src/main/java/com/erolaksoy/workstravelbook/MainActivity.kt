2218d5787687d057653d61a56a8da7a2f4595dbb
pcparticle <erolaxoy98@gmail.com>
2020-08-06 16:31:58 +0300

    SqLiteMainActivityShowingAdress

--- app/src/main/java/com/erolaksoy/workstravelbook/MainActivity.kt
+++ app/src/main/java/com/erolaksoy/workstravelbook/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.erolaksoy.workstravelbook
 
+import android.content.Context
 import android.content.Intent
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +29,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //SQLITE OPERATIONS
+        try{
+            val database = openOrCreateDatabase("Places", Context.MODE_PRIVATE,null)
+            val myCursor =database.rawQuery("Select * From places",null)
+            val adressIndex =myCursor.getColumnIndex("adress")
+            val latitudeIndex =myCursor.getColumnIndex("latitude")
+            val longitudeIndex =myCursor.getColumnIndex("longitude")
+
+            while(myCursor.moveToNext()) {
+                val adressFromDatabase = myCursor.getString(adressIndex)
+                val latitudeFromDatabase = myCursor.getDouble(latitudeIndex)
+                val longitudeFromDatabase = myCursor.getDouble(longitudeIndex)
+                val myCurrentSavedPlace = Place(adressFromDatabase,latitudeFromDatabase,longitudeFromDatabase)
+                println(myCurrentSavedPlace.adress + myCurrentSavedPlace.latitude)
+            }
+            myCursor.close()
+
+
+        }catch(e : Exception){e.printStackTrace()}
+
     }
 
 }
